2e8baeb558029865d1328385ee2170077e84fe14
Kwai Liu <[cmdrtomalak@gmail.com](mailto:cmdrtomalak@gmail.com)>
2022-12-03 12:52:14 -0500

    Python variable names

--- day1/day1_py/solution.py
+++ day1/day1_py/solution.py
@@ -1,15 +1,17 @@
-maxCals = 0
-calsOfCurrentReindeer = 0
+#!/usr/bin/python3
+
+max_cals = 0
+cals_of_current_inventory = 0
 with open('elf_calories.txt', 'r') as f:
     for line in f:
         if line == '\n':
-            maxCals = max(maxCals, calsOfCurrentReindeer)
-            calsOfCurrentReindeer = 0
+            max_cals = max(max_cals, cals_of_current_inventory)
+            cals_of_current_inventory = 0
             continue
         try:
             cals = int(line)
         except Exception:
             raise Exception(f'line {line} not a valid positive number')
         
-        calsOfCurrentReindeer += cals 
+        cals_of_current_inventory += cals 
 print(maxCals)
