2b412acd8b5c95652cd3d7943d26d10b90fd8b8c
Kwai Liu <[cmdrtomalak@gmail.com](mailto:cmdrtomalak@gmail.com)>
2022-12-03 12:42:25 -0500

    Remove hashmap, use sorted vector

--- day1/day1_rs/src/main.rs
+++ day1/day1_rs/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
@@ -8,34 +7,30 @@ fn main() -> io::Result<()> {
     let file = File::open(ELF_CALORIES)?;
     let reader = BufReader::new(file);
 
-    let mut records = HashMap::new();
-    let mut elf_count = 1;
+    let mut elf_vec: Vec<i32> = vec![];
 
+    let mut calories = 0;
     for line in reader.lines() {
         let value = line?;
         if !value.as_str().trim().is_empty() {
-            // println!("{value}")
-            *records.entry(elf_count).or_insert(0) += value.parse::<i32>().unwrap();
+            calories += value.parse::<i32>().unwrap();
         } else {
-            elf_count += 1;
+            // add entry at every breakline
+            elf_vec.push(calories);
+            calories = 0;
         }
     }
 
+    // sort vec highest to lowest
+    elf_vec.sort_by(|a, b| b.cmp(a));
     // Part 1: How many total Calories is that Elf carrying?
-    let elf_with_max_calories = records.iter().max_by_key(|entry| entry.1).unwrap();
-    println!("Elf number: {}", &elf_with_max_calories.0);
-    println!(
-        "Is carrying a total of {} calories",
-        &elf_with_max_calories.1
-    );
+    println!("Top Elf is carrying a total of {} calories", elf_vec[0]);
 
     // To sort, convert it to a vector
-    let mut hash_vec: Vec<_> = records.iter().collect();
-    hash_vec.sort_by(|a, b| b.1.cmp(a.1));
 
     let mut sum = 0;
     for i in 0..3 {
-        sum += hash_vec[i].1;
+        sum += elf_vec[i];
     }
 
     println!("Total calories carried by top 3 Elfs: {sum}");
