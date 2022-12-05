cd195e0fecc200ce643ef1e7ff9f14fd40815b52
Kwai Liu <[cmdrtomalak@gmail.com](mailto:cmdrtomalak@gmail.com)>
2022-12-05 09:57:07 -0500

    Experiment with Hashmaps

--- day3/badge_rs/src/main.rs
+++ day3/badge_rs/src/main.rs
@@ -2,6 +2,25 @@ use std::collections::HashMap;
 
 const INPUT: &str = include_str!("../input.txt");
 
+fn get_priority(item: &char) -> usize {
+    let lower: HashMap<char, usize> = ('a'..='z')
+        .into_iter()
+        .map(|c| (c, c as usize - 96))
+        .collect();
+
+    let upper: HashMap<char, usize> = ('A'..='Z')
+        .into_iter()
+        .map(|c| (c, c as usize - 38))
+        .collect();
+
+    dbg!(lower);
+    dbg!(upper);
+
+    5
+}
+
 fn main() {
-    println!("{INPUT}");
+    let input: Vec<&str> = INPUT.lines().collect();
+
+    get_priority(&'a');
 }
