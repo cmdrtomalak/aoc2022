ec1cbcfd91342ad6ae367994b3f00c689f4dba46
Kwai Liu <[cmdrtomalak@gmail.com](mailto:cmdrtomalak@gmail.com)>
2022-12-10 15:23:00 -0500

    got text input

--- day5/move_rs/src/main.rs
+++ day5/move_rs/src/main.rs
@@ -1,28 +1,58 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use std::fs;
+use std::str::FromStr;
 
+/*
 fn read_file(filename: &str) -> Result<(Vec<String>, Vec<String>)> {
     let data = fs::read_to_string(filename).unwrap();
 
     let mut split = data.split("\n\n");
     let stack_text = split
         .next()
-        .ok_or("")
-        .to_owned()
-        .trim()
-        .split("\n")
-        .collect::<Vec<String>>();
+        .ok_or_else(|| anyhow!("Failed to gt stack text from input file"))
+        .and_then(|s| {
+            s.trim()
+                .split("\n")
+                .map(|s| String::from_str(s.trim()))
+                .collect()
+        })?;
     let movements = split
         .next()
-        .ok_or("")
-        .to_owned()
-        .trim()
-        .split("\n")
+        .ok_or_else(|| anyhow!("Failed to get movements from input file"))
         .collect::<Vec<String>>();
 
     Ok((stack_text, movements))
 }
+*/
+
+fn read_file(filename: &str) -> Result<(Vec<String>, Vec<String>)> {
+    let data = fs::read_to_string(filename)?;
+
+    let mut split = data.split("\n\n");
+    let stack_text = split
+        .next()
+        .ok_or_else(|| anyhow!("Failed to get stack text from input file"))
+        .and_then(|s| {
+            Ok(s.trim()
+                .split("\n")
+                .map(|s| String::from_str(s.trim()))
+                .collect::<Result<Vec<_>, _>>())
+        })?;
+    let movements = split
+        .next()
+        .ok_or_else(|| anyhow!("Failed to get movements from input file"))
+        .and_then(|s| {
+            Ok(s.trim()
+                .split("\n")
+                .map(|s| String::from_str(s.trim()))
+                .collect::<Result<Vec<_>, _>>())
+        })?;
+
+    Ok((stack_text.unwrap(), movements.unwrap()))
+}
 
 fn main() {
-    read_file("input.txt");
+    let (stack_text, movements) = read_file("input.txt").unwrap();
+    dbg!(&stack_text);
+    dbg!(&movements);
 }
