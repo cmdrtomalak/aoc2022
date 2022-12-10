4a93ecbb20128cc73dc333aec8172625f7e311b8
Kwai Liu <[cmdrtomalak@gmail.com](mailto:cmdrtomalak@gmail.com)>
2022-12-10 08:34:57 -0500

    Finished Part 4 in Rust

--- day4/clear_rs/src/main.rs
+++ day4/clear_rs/src/main.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 use itertools::Itertools;
-use std::collections::HashSet;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
@@ -16,28 +15,48 @@ fn read_file(filename: &str) -> Result<Vec<String>> {
     Ok(data)
 }
 
-fn parse(raw: &Vec<String>) {
-    /*
-    for line in raw {
-        for elf in line.split(",") {
-            println!("{elf}");
-        }
-    }
-    */
-    let parsed: Vec<(usize, usize, usize, usize)> = raw
-        .iter()
+fn parse(raw: &Vec<String>) -> Vec<(usize, usize, usize, usize)> {
+    raw.iter()
         .map(|l| {
             l.split(['-', ','])
                 .map(|v| v.parse::<usize>().unwrap())
                 .collect_tuple::<(_, _, _, _)>()
                 .unwrap()
         })
-        .collect();
-
-    dbg!(parsed);
+        .collect()
 }
 
 fn main() {
     let raw = read_file("input.txt").unwrap();
-    parse(&raw);
+    let input = parse(&raw);
+    let contain: i32 = input
+        .iter()
+        .map(|(a1, a2, b1, b2)| {
+            if a1 <= b1 && b2 <= a2 {
+                // b is a subset of a
+                1
+            } else if b1 <= a1 && a2 <= b2 {
+                // a is a subset of b
+                1
+            } else {
+                0
+            }
+        })
+        .sum();
+
+    let overlap: i32 = input
+        .iter()
+        .map(|(a1, a2, b1, b2)| {
+            if b1 <= a2 && a1 <= b2 {
+                1
+            } else if a1 <= b2 && b1 <= a2 {
+                1
+            } else {
+                0
+            }
+        })
+        .sum();
+
+    println!("Part 1: {contain}");
+    println!("Part 2: {overlap}");
 }
